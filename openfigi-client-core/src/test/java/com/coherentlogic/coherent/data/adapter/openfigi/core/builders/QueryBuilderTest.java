4f46d06861582d0657a3d8764892a53ed715fa6c
thospfuller <thomas.fuller@coherentlogic.com>
2016-10-11 12:08:23 -0400

    These tests are now being ignored.

--- openfigi-client-core/src/test/java/com/coherentlogic/coherent/data/adapter/openfigi/core/builders/QueryBuilderTest.java
+++ openfigi-client-core/src/test/java/com/coherentlogic/coherent/data/adapter/openfigi/core/builders/QueryBuilderTest.java
@@ -1,38 +1,39 @@
 package com.coherentlogic.coherent.data.adapter.openfigi.core.builders;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
  * 
  * @author <a href="support@coherentlogic.com">Support</a>
  */
+@Ignore
 public class QueryBuilderTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test
-	public void testGetRequestBody() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetRequestBody() {
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testGetHeaders() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testGetHeaders() {
+        fail("Not yet implemented");
+    }
 
-	@Test
-	public void testWithApiKey() {
-		fail("Not yet implemented");
-	}
-
+    @Test
+    public void testWithApiKey() {
+        fail("Not yet implemented");
+    }
 }
